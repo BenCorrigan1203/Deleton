2418fa0379837795a2da1e1eb3eba0ceebb26fa1
Zak-Simjee <88289386+Zak-Simjee@users.noreply.github.com>
2023-05-17 15:25:17 +0100

    working on recent rides

--- live_dashboard/dash_utils.py
+++ live_dashboard/dash_utils.py
@@ -1,7 +1,11 @@
 import os
+from datetime import datetime
 
+import pandas as pd
 import psycopg2
 from sqlalchemy import URL, create_engine
+from dotenv import load_dotenv
+
 
 def get_db_connection():
     """Connects to the database"""
@@ -18,3 +22,27 @@ def get_db_connection():
     except Exception as err:
         print(err)
         print("Error connecting to database.")
+
+load_dotenv()
+
+conn = get_db_connection()
+
+ride_df = pd.read_sql_table("ride", conn)
+
+rider_df = pd.read_sql_table("rider", conn)
+
+past_12_hours = datetime.fromtimestamp(datetime.now().timestamp() - (12 * 3600)).hour
+
+recent_rides = ride_df[ride_df["start_time"].dt.hour > past_12_hours].dropna()
+
+recent_rides["gender"] = recent_rides["rider_id"].apply(lambda x: rider_df["gender"].where(rider_df["rider_id"] == x).dropna().values[0])
+
+print(recent_rides["gender"])
+
+recent_rides["total_duration"] = recent_rides["start_time"].apply()
+
+print(recent_rides[["start_time", "end_time"]])
+
+
+
+
