2dfde31f52ba22d3781d24299f24428a5218af6f
abdirrahmansl <trainee.abdirrahman.mohamed@sigmalabs.co.uk>
2023-05-23 14:27:22 +0100

    fix deloton.sh

--- deloton.sh
+++ deloton.sh
@@ -9,8 +9,8 @@ terraform plan -target=aws_ecr_repository.live-dash -target=aws_ecr_repository.c
 terraform apply -target=aws_ecr_repository.live-dash -target=aws_ecr_repository.compress -target=aws_ecr_repository.compress -target=aws_ecr_repository.API
 
 # Create the RDS
-terraform plan -target=aws_db_instance.deleton-rds
-terraform apply -target=aws_db_instance.deleton-rds
+terraform plan -target=aws_db_instance.deloton-rds
+terraform apply -target=aws_db_instance.deloton-rds
 
 # Login into AWS
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 605126261673.dkr.ecr.eu-west-2.amazonaws.com
@@ -18,47 +18,47 @@ aws ecr get-login-password --region eu-west-2 | docker login --username AWS --pa
 cd ../api
 # Dockerize the API
 # Create docker image for the API
-docker build -t c7-deleton-api .
+docker build -t c7-deloton-api .
 # Tag docker image 
-docker tag c7-deleton-api:latest 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deleton-api:latest
+docker tag c7-deloton-api:latest 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deloton-api:latest
 # Deploy docker image to the ECR
-docker push 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deleton-api:latest
+docker push 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deloton-api:latest
 
 cd ../compress_data
 # Dockerize the compression script
 # Create docker image for the compression script
-docker build -t c7-deleton-compression-script .
+docker build -t c7-deloton-compression-script .
 # Tag docker image 
-docker tag c7-deleton-compression-script:latest 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deleton-compression-script:latest
+docker tag c7-deloton-compression-script:latest 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deloton-compression-script:latest
 # Deploy docker image to the ECR
-docker push 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deleton-compression-script:latest
+docker push 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deloton-compression-script:latest
 
 cd ../ingestion
 # Dockerize the ingestion script
 # Create docker image for the ingestion script
-docker build -t c7-deleton-ingestion-script .
+docker build -t c7-deloton-ingestion-script .
 # Tag docker image ingestion script
-docker tag c7-deleton-ingestion-script:latest 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deleton-ingestion-script:latest
+docker tag c7-deloton-ingestion-script:latest 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deloton-ingestion-script:latest
 # Deploy docker image to the ECR
-docker push 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deleton-ingestion-script:latest
+docker push 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deloton-ingestion-script:latest
 
 cd ../live_dashboard
 # Dockerize the live dashbaord
 # Create docker image for the live dashbaord
-docker build -t c7-deleton-live-dashboard .
+docker build -t c7-deloton-live-dashboard .
 # Tag docker image 
-docker tag c7-deleton-live-dashboard:latest 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deleton-live-dashboard:latest
+docker tag c7-deloton-live-dashboard:latest 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deloton-live-dashboard:latest
 # Deploy docker image to the ECR
-docker push 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deleton-live-dashboard:latest
+docker push 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deloton-live-dashboard:latest
 
 cd ../report_generation
 # Dockerize the genearation script
 # Create docker image for the genearation script
-docker build -t c7-deleton-report-generation .
+docker build -t c7-deloton-report-generation .
 # Tag docker image 
-docker tag c7-deleton-report-generation:latest 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deleton-report-generation:latest
+docker tag c7-deloton-report-generation:latest 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deloton-report-generation:latest
 # Deploy docker image to the ECR
-docker push 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deleton-report-generation:latest
+docker push 605126261673.dkr.ecr.eu-west-2.amazonaws.com/c7-deloton-report-generation:latest
 
 cd ../terraform
 # Create Lambda and Step functions, ECS cluster, Task Definition and Services with Load Balancers. 
